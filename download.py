44b1e34d068155d5a126814b3596bf2062d55dbb
Ram Vasuthevan <36580682+RamVasuthevan@users.noreply.github.com>
2023-07-27 19:55:26 +0000

    Run Black

--- download.py
+++ download.py
@@ -46,7 +46,9 @@ for year in YEARS:
         file_content = file_response.text.strip().replace(DELIMITER[year], "\t")
         lines = file_content.split("\n")
 
-        with open(os.path.join(OUTPUT_DIRS[year], link.replace(".txt", ".tsv")), "w") as state_file:
+        with open(
+            os.path.join(OUTPUT_DIRS[year], link.replace(".txt", ".tsv")), "w"
+        ) as state_file:
             # Add headers to the individual state files
             if HEADERS[year]:
                 state_file.write(HEADERS[year] + "\n")
